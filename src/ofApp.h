37f0c13b365c9c000c14f570ce36b1e50093e14c
Mathew Gutoskie <98121688+mathewgut@users.noreply.github.com>
2024-11-29 16:12:42 -0500

    added basic colour tracking, remapped position values to work with 720p, added supporting test images

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,10 +1,71 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxImGui.h"
+#include "ImHelpers.h"		//this gives us some extra ofxImGui features (needed for drop-down menus)
+
+#include "CVC.h"
 
 class ofApp : public ofBaseApp{
 
 	public:
+		ofVideoPlayer m_videoPlayer;
+		ofVideoGrabber m_videoGrabber;
+		bool m_videoResolutionConflict;		//do we need to re-size our video before we process?
+		bool m_camResolutionConflict;		//do we need to re-size our video before we process?
+		bool m_camPaused;					//pausing our camera feed
+
+		bool debug = true;
+
+		/* BOUNDRY PADDING */
+		float boundryPadding = 50.0f;
+		float spawnedObjBoundryPadding = 10.0f;
+		float buttonBoundryPadding = 20.0f;
+
+		/* HANDS */
+		float handSize = 100.0f;
+
+		ofxCvContourFinder m_hand1;
+
+		ofVec2f hand1Center;
+		ofVec2f hand1TL;
+		ofVec2f hand1BR;
+
+		ofImage handImage;
+
+		/* dark area */
+		ofImage darkAreaImage;
+
+		/* background */
+		ofImage backgroundImage;
+
+
+		//CV images
+		ofxCvColorImage		m_colorImage;
+		ofxCvGrayscaleImage	m_grayscaleDiffImage;
+		ofxCvGrayscaleImage	m_grayscaleDiffImage2;
+
+		//GUI
+		ofxImGui::Gui		m_gui;
+		int					m_threshold;	//how closely does the colour have to match to track?
+		int					m_numContoursConsidered;
+		float				m_minArea;
+		float				m_maxArea;
+
+		//we want to select which color we are tracking within the video
+		float m_trackedColor[4] = { 0.0f, 0.0f, 0.0f, 0.0f };	//ofxImgui requires color in this form
+		float m_trackedColor2[4] = { 0.0f, 0.0f, 0.0f, 0.0f };	//ofxImgui requires color in this form
+
+		//app modes
+		CVC::APP_MODE m_appMode;				//current app state
+		std::vector<std::string> m_appModes;	//"list"/dynamic array of app states
+
+
+		//we will do all color CV processing here
+		//we are passing a reference here ...
+		void processColor(ofxCvColorImage& image);
+
 		void setup();
 		void update();
 		void draw();
@@ -20,5 +81,9 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+		bool collisionCheck(ofVec2f obj1TL, ofVec2f obj1BR, ofVec2f obj2TL, ofVec2f obj2BR);
+		void setHandBoundries();
+		void setSpawnedObjBoundries();
 		
 };
--- src/search-game-methods.cpp
+++ src/search-game-methods.cpp
@@ -2,16 +2,85 @@
 #include "ofApp.h"
 
 
-void ofApp::setup() {
+// basic collision check between two object's lowest and highest points. if another object's point is within that range, return true
+bool ofApp::collisionCheck(ofVec2f obj1TL, ofVec2f obj1BR, ofVec2f obj2TL, ofVec2f obj2BR) {
+    //std::cout << "obj1TL: (" << obj1TL.x << ", " << obj1TL.y << "), obj1BR: (" << obj1BR.x << ", " << obj1BR.y << ")\n";
+   // std::cout << "obj2TL: (" << obj2TL.x << ", " << obj2TL.y << "), obj2BR: (" << obj2BR.x << ", " << obj2BR.y << ")\n";
+
+    if (obj1TL.x < obj2BR.x && obj1BR.x > obj2TL.x &&
+        obj1TL.y < obj2BR.y && obj1BR.y > obj2TL.y) {
+        //std::cout << "YES.\n";
+        return true;
+    }
+
+    else {
+        //std::cout << "NO.\n";
+        return false;
+    }
+}
+
+// sets boundries for spawnedObj based on position
+void ofApp::setSpawnedObjBoundries() {
+    //spawnedObjTL.x = spawnedObjPos.x - spawnedObjBoundryPadding;
+    //spawnedObjTL.y = spawnedObjPos.y - spawnedObjBoundryPadding + 10;
+
+    // set bottom right points x and y based off center point and size of image
+    //spawnedObjBR.x = spawnedObjPos.x + spawnedObjBoundryPadding;
+    //spawnedObjBR.y = spawnedObjPos.y + spawnedObjBoundryPadding - 10;
 
 }
 
-//--------------------------------------------------------------
-void ofApp::update() {
+
+// sets boundries for hand1 and hand2 based on position
+void ofApp::setHandBoundries() {
+    // set top left points x and y based off center point and size of image
+    //hand1TL.x = hand1Center.x - boundryPadding;
+
+    //hand1TL.y = hand1Center.y - boundryPadding + 15; // altering vertical y so collision works better
+
+    // set bottom right points x and y based off center point and size of image
+    //hand1BR.x = hand1Center.x + boundryPadding;
+
+    //hand1BR.y = hand1Center.y + boundryPadding - 15;
+
 
 }
 
-//--------------------------------------------------------------
-void ofApp::draw() {
+void ofApp::processColor(ofxCvColorImage& image) {
+    const int numChannelsPerPixel = image.getPixels().getNumChannels(); //should be 3 for red, green, blue channels
+    const int numChannels = CVC::VIDEO_WIDTH * CVC::VIDEO_HEIGHT * numChannelsPerPixel;
+    float pixel[3] = { 0.0f, 0.0f, 0.0f };
+
+    //loop through color image pixels
+    //check if pixel color matches our tracked color
+    //if true, set our diff image pixel color to white
+    //if false, set our diff image pixel color to black
+    for (int i = 0; i < numChannels; i += numChannelsPerPixel) {
+        pixel[0] = image.getPixels()[i + 0];        //red
+        pixel[1] = image.getPixels()[i + 1];        //green
+        pixel[2] = image.getPixels()[i + 2];        //blue
+
+        //check if tracked color matches pixel color
+        //m_trackedColor is a float color [0.0, 1.0] so we need to convert back to a byte color [0,255]
+        if ((abs(pixel[0] - m_trackedColor[0] * 255.0f) < m_threshold) &&
+            (abs(pixel[1] - m_trackedColor[1] * 255.0f) < m_threshold) &&
+            (abs(pixel[2] - m_trackedColor[2] * 255.0f) < m_threshold)) {
+
+            //COLOR THE SAME - YAAYYYYYYY!!
+            m_grayscaleDiffImage.getPixels()[i / numChannelsPerPixel] = 255;
+        }
+        else {
+            //NOT THE SAME - BOOO
+            m_grayscaleDiffImage.getPixels()[i / numChannelsPerPixel] = 0;
+        }
+
+
+    }
+
+    //update cv image
+    m_grayscaleDiffImage.flagImageChanged();        //tells the compy that the image has changed on the CPU RAM and should be moved to GPU RAM
+
+    //find contours/blobs
+    m_hand1.findContours(m_grayscaleDiffImage, m_minArea, m_maxArea, m_numContoursConsidered, false, true);
 
 }
