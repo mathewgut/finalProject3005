27c0c4c31b4d2614b7daedce2a6b6a3319fa3471
p s <122562596+prototaxi@users.noreply.github.com>
2024-12-04 14:16:49 -0500

    Update ofApp.cpp
    update() functions are in radio-game-method.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,66 @@
 #include "ofApp.h"
+#include "radio-game-method.cpp"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetVerticalSync(true);
+	ofBackground(0);
 
+	//connect to arduino
+	serial.listDevices();
+	vector <ofSerialDeviceInfo> deviceList = serial.getDeviceList();
+
+	int baud = 9600;
+	serial.setup("COM3", baud);
+
+	//initialize values
+	stringData = "";
+	potValue = "";
+
+	fadeValue = 0.0f;
+	volume1 = 0.0f;
+	volume2 = 0.0f;
+	volume3 = 0.0f;
+	volume4 = 0.0f;
+
+	//load and play sound sources
+	sound_static.load("static.mp3");
+	sound_brightside.load("brightside.mp3");
+	sound_station1.load("echoLima.mp3");
+	sound_station2.load("conet333.mp3");
+
+	sound_static.setLoop(true);
+	sound_brightside.setLoop(true);
+	sound_station1.setLoop(true);
+	sound_station2.setLoop(true);
+
+	sound_static.play();
+	sound_brightside.play();
+	sound_station1.play();
+	sound_station2.play();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+	readPotentiometer();
 
+	radioFader(float fadeValue, float volume1, float volume2, float volume3, float volume4);
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 
-}
+	ofSetColor(255);
+	ofDrawBitmapString("potentiometer value: " + ofToString(potValue), 20, 20);
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+	//DEBUG: display mouse input, volume vars, and fadeValue
+	int mouseX = ofGetMouseX();
+	ofDrawBitmapString("mouse pos: " + ofToString(mouseX), 20, 40);
 
-}
+	ofDrawBitmapString("volume (static): " + ofToString(volume1), 20, 60);
+	ofDrawBitmapString("volume (brightside): " + ofToString(volume2), 20, 80);
+	ofDrawBitmapString("volume (station1): " + ofToString(volume3), 20, 100);
+	ofDrawBitmapString("volume (station2): " + ofToString(volume4), 20, 120);
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+	ofDrawBitmapString("fadeValue: " + ofToString(fadeValue), 20, 140);
 }
