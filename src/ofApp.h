d1f85441f4f71383493a36f969c6534a6ef6fb3a
p s <122562596+prototaxi@users.noreply.github.com>
2024-12-04 16:20:57 -0500

    added arduino vars to ofapp.h

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofEvents.h"
+
+namespace config {
+    static const std::string ARDUINO_DEVICE_NAME = "COM3";  // Update with your device name
+};
 
 class ofApp : public ofBaseApp{
 
 	public:
+		static const int PIN_BUTTON_5 = 6; // Button 5 on digital pin 6
 		void setup();
 		void update();
 		void draw();
@@ -13,6 +19,15 @@ class ofApp : public ofBaseApp{
 		string				potValue;
 		int				byteData;
 		string				stringData;
+	
+	   	 // Arduino stuff
+	   	 ofArduino m_arduino;
+	    	int m_code[4] = { 0, 0, 0, 0 };  // The 4-digit code, initialized to 0
+	    	bool m_bSetup;                 // Is Arduino initialized?
+
+		void setupArduino(const int& _version);
+		void updateArduino();
+		void digitalPinChanged(const int& pinNum);
 
 		ofSoundPlayer			sound_static;
 		ofSoundPlayer			sound_brightside;
@@ -28,5 +43,5 @@ class ofApp : public ofBaseApp{
 		
 
 		void readPotentiometer();
-		void radioFader(float fadeValue, float volume1, float volume2, float volume3, float volume4);
+		bool radioFader(float fadeValue, float volume1, float volume2, float volume3, float volume4, int PIN_BUTTON_5, ofArduino m_arduino);
 };
